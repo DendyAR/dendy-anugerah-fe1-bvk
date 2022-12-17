1c2bcf3e3d3c61cc00df59ee23815bdd406f19f5
DendyAR <anugerah700@gmail.com>
2022-12-18 01:03:49 +0700

    done

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,6 @@
 import React, {useState } from "react";
 import useCatsSearch from "../components/useCatsSearch";
 import Card from "../components/Card";
-import { Link } from "react-router-dom";
 
 export default function Home() {
   const [query, setQuery] = useState("");
